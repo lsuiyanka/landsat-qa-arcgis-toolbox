d2169ca6d46741f37aa049d1e199fc74e0f0fe67
stevefoga <steven.foga.ctr@usgs.gov>
2017-08-24 08:52:10 -0500

    updated to use bit-wise operations instead of value dictionary. includes new logic for writing out radsat_qa labels cleanly, and not duplicating labels when double bits are set to yes.

--- Scripts/qa_decode.py
+++ Scripts/qa_decode.py
@@ -37,7 +37,7 @@ def build_attr_table(raster_in, sensor, band):
     :return:
     """
     # read lookup dictionary
-    qa_values = lookup_dict.qa_values
+    bit_flags = lookup_dict.bit_flags
 
     # check to ensure raster is not floating/double/complex
     vt = int(str(arcpy.GetRasterProperties_management(raster_in, "VALUETYPE")))
@@ -65,21 +65,97 @@ def build_attr_table(raster_in, sensor, band):
     # assign values to attribute table
     with arcpy.da.UpdateCursor(raster_in, fields) as cursor:
         for row in cursor:
-            try:
-                if band == "radsat_qa":  # radsat_qa is sensor-agnostic
-                    row[1] = qa_values[band][row[0]]
-                elif sens == 'L8' and band == "pixel_qa" \
-                        and row[0] >= 1024:  # L8 terrain occlusion
-                    row[1] = 'Terrain occlusion'
+            # check all possible bits for match with target value (row[0])
+            bit_values = sorted(bit_flags[band][sens].values())
+            bit_bool = []
+            for bv in bit_values:
+                if len(bv) == 1:  # single bit
+                    bit_bool.append(row[0] & 1 << bv[0] > 0)
+
+                elif len(bv) > 1:  # 2+ bits
+                    bits = []
+                    for b in bv:
+                        bits.append(row[0] & 1 << b > 0)
+                    if all(item == True for item in bits):
+                        bit_bool.append(True)
+                    else:
+                        bit_bool.append(False)
+
                 else:
-                    row[1] = qa_values[band][sens][row[0]]
+                    sys.exit("No valid bits found for target band.")
 
-                cursor.updateRow(row)
+            # create description of each value based upon all possible bits
+            true_bits = [i for (i, bb) in zip(bit_values, bit_bool) if bb]
 
-            except KeyError:
-                arcpy.AddError("ERROR: Band value {0} in file {1} does not "
-                               "have a corresponding key value for {2} band."
-                               .format(row[0], raster_in, band))
+            # if double bits exist, eliminate single bit descriptions,
+            #   otherwise, the descriptions will duplicate themselves.
+            bb_double = [len(i) > 1 for i in true_bits]
+            if any(bb_double):
+                # get only the double bits
+                dbit_nest = [i for (i, db) in zip(true_bits, bb_double) if db]
+
+                # collapse the bits into a single list
+                dbits = [item for sublist in dbit_nest for item in sublist]
+
+                # remove matching single bits out of true_bits list
+                tbo = []
+                for t in true_bits:
+                    tb_out = []
+                    for d in dbits:
+                        if t[0] != d or len(t) > 1:
+                            tb_out.append(True)
+                        else:
+                            tb_out.append(False)
+                    if all(tb_out):
+                        tbo.append(t)
+
+                # replace true_bits with filtered list
+                true_bits = tbo
+
+            # get description of each bit, and create output string
+            for tb in range(len(true_bits)):
+                k = next(key for key, value in bit_flags[band][sens].items()
+                         if value == true_bits[tb])
+
+                # TODO: add logic to remove 'low' shadow, snow/ice, cloud classifications
+                # TODO: NOTE - low aerosol should be flagged.
+                # TODO: NOTE - if all (BQA or PIXELQA) is 'low', label 'clear'
+                # If radsat_qa, handle differently to make display cleaner
+                if band == 'radsat_qa':
+                    if 'k' not in vars():
+                        desc = 'No Saturation'
+
+                    else:
+                        if tb == 0:
+                            desc = "Band {0} Data Saturation".format(
+                                true_bits[tb][0])
+
+                        else:
+                            desc = "{0},{1} Data Saturation".format(
+                                desc[:desc.find('Data')-1], true_bits[tb][0])
+
+                # handle situations where no bits are selected
+                elif band == 'sr_cloud_qa' or band == 'sr_aerosol' and not k:
+                    desc = 'None'
+
+                elif band == 'BQA' and not k:
+                    desc = 'Not Determined'
+
+                # string creation for all other bands
+                else:
+                    if tb == 0:
+                        desc = "{0}".format(k)
+                    else:
+                        desc += ", {0}".format(k)
+
+            # add desc to row description (row[1])
+            try:
+                row[1] = desc
+            except UnboundLocalError:
+                row[1] = 'ERROR: bit read incorrectly'
+
+            # update table row
+            cursor.updateRow(row)
 
     # if running in ArcMap, refresh display in current mxd
     try:
